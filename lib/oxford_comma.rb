5d9b930771c249930f36ef0b759bcd10cae3522d
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-04-28 21:44:26 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return array.join(" and ")
   elsif array.length >= 3
     array[array.length - 1].insert("and")
-    array.join(", ")
+    puts array.join(", ")
   else
     return array.join
   end
