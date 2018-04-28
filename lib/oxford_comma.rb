a7a6b7934f85d030807cb9a584f84e4b12444f69
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-04-28 21:58:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,9 @@
 def oxford_comma(array)
-  return array.to_s if array.nil? or array.length <= 1
-  array[0..-2].join(", ") + " and " + array[-1]
+  if array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    return array[0..-2].join(", ") + " and " + array[-1]
+  else
+    return array.join
+  end
 end
