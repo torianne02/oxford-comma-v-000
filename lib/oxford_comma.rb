8a3241dc4f1b273c1696603fda648471ee0a3266
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-04-28 21:59:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    return array[0..-2].join(", ") + ", and " + array[-1]
+  else
+    return array.join
+  end
 end
